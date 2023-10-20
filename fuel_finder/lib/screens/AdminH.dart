65ff61be3999fe4b3f7223dae6514b55ab233094
it21104176 <it21104176@my.sliit.lk>
2023-10-20 15:05:41 +0530

    'Add_user_interface'

---
+++ fuel_finder/lib/screens/AdminH.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:fuel_finder/screens/showall.dart';
+
+class AdminHomePage extends StatefulWidget {
+  @override
+  _AdminHomePageState createState() => _AdminHomePageState();
+}
+
+class _AdminHomePageState extends State<AdminHomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Users'),
+    showall(),
+    Text('App Home'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  String _getAppBarTitle(int index) {
+    if (index == 0) {
+      return 'Admin Home - Users';
+    } else if (index == 1) {
+      return 'Admin Home - Gas';
+    } else if (index == 2) {
+      return 'Admin Home - App Home';
+    }
+    return 'Admin Home'; // Default title
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_getAppBarTitle(_selectedIndex)),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Users',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_gas_station),
+            label: 'Gas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'App Home',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
---
+++ fuel_finder/lib/screens/showall.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fuel_finder/screens/AddStation.dart';
+import 'package:fuel_finder/screens/DetailPage.dart';
+
+
+class showall extends StatefulWidget {
+  const showall({Key? key}) : super(key: key);
+
+  @override
+  _showallState createState() => _showallState();
+}
+
+class _showallState extends State<showall> {
+  final CollectionReference _stations = FirebaseFirestore.instance.collection('stations');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: _stations.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                final String sname = documentSnapshot['SName']; // Extract name
+                final String location = documentSnapshot['Location']; // Extract description
+                final String stationId = documentSnapshot.id;
+                return GestureDetector(
+                  onTap: () {
+                    // Navigate to the details page when a card is tapped.
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DetailPage(stationId : stationId),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(sname),
+                      subtitle: Text(location),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
